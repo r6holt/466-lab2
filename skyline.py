c46260526051bae027a18682617a290c1070e6b9
Grayson <gclenden@calpoly.edu>
2019-10-02 14:00:14 -0700

    apriori rewrite begins

--- skyline.py
+++ skyline.py
@@ -2,25 +2,28 @@ from importer import Importer
 import sys
 import pandas as pd
 
-'''T is the market baskets, I is the items, minSup is the threshold for support'''
+'''data is the market baskets, items is the items, minSup is the threshold for support'''
 
 MIN_SUPPORT = .1
 MIN_CONF = .6
 goods = [0]*50
 max_rows = 0
-candidates = []
+candidates = [[], []]
+counts = []
 
 def main():
    #  inf = open(sys.argv[1], 'r')
    #  print(sys.argv[1])
    im = Importer()
    example1 = im.import_sparse("data/example/out1.csv")
-   support(example1)
+   support_1(example1)
+   fillCandidates(2)
 
-   print(candidates)
+   for row in candidates:
+       print(row)
+   #print(candidates)
 
-
-def support(data):
+def support_1(data, n):
    max_rows = 0
    for row in data.iterrows():
       getCount(row[1])
@@ -30,9 +33,22 @@ def support(data):
    while i < len(goods):
       print(i, goods[i], max_rows)
       if float(goods[i])/float(max_rows) > MIN_SUPPORT:
-         candidates.append(i)
+         candidates[1].append(i)
       i += 1
-   
+
+def support_n(my_set, n)
+   max_rows = 0
+   for row in data.iterrows():
+      getCount(row[1])
+      max_rows += 1
+
+   i = 0
+   while i < len(goods):
+      print(i, goods[i], max_rows)
+      if float(goods[i])/float(max_rows) > MIN_SUPPORT:
+         candidates[1].append(i)
+      i += 1
+
 
 # def support2(T, X, Y):
 #     F[0] = support(T, I, minSup)
@@ -53,17 +69,14 @@ def support(data):
     
 
 
-#         if(F[k]==0):
-#             break
-
 '''   
-   total = T["Id"].max()
+   total = data["itemsd"].max()
     
-    print(T['1'].value_counts())
+    print(data['1'].value_counts())
 
     support = 0
 
-    print("Total: ", total, "\nCount: ", count, "\nSupport: ", support)
+    print("dataotal: ", total, "\ncandidatesount: ", count, "\nSupport: ", support)
 '''
 def getCount(row):
     i = 1
@@ -71,6 +84,13 @@ def getCount(row):
       if not pd.isna(row[i]):
          goods[int(row[i])] += 1
       i += 1
+
+def fillCandidates(n):
+    temp = frequent[n-1]
+    candidates.append([])
+    for i in range(len(temp)-1):
+        for j in range(i+1, len(temp)):
+            candidates[n] = candidates[n] + [[temp[i], temp[j]]]
 
 
 if __name__ == "__main__":
